2c2ca0fcf4f5fd24344edea027e70eb58e1dbd79
nmq6 <nmq6@txstate.edu>
2023-10-20 13:43:20 -0500

    modifying 2 files

--- src/project2/FireballScroll.java
+++ src/project2/FireballScroll.java
@@ -1,5 +1,14 @@
 package project2;
 
 public class FireballScroll {
-
+	
+	private final int PERCENT = 100;
+	
+	public int calculateDamage(int hit) {
+		return PERCENT - hit;
+	}
+	
+	public void graphicalEffect() {
+		
+	}
 }
