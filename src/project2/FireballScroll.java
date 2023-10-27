7b83330df796fc52984b36cbe7a3691623738d55
b.schlueter <schlueter.braden@gmail.com>
2023-10-27 12:38:07 -0500

    removed comments from fireball scroll

--- src/project2/FireballScroll.java
+++ src/project2/FireballScroll.java
@@ -2,14 +2,12 @@ package project2;
 
 public class FireballScroll implements Abilities{
 	
-	//private final int PERCENT = 100; Shouldnt need this now since 
-    //  we added the Abilities implementation
 	
 	public int calculateDamage(int hit) {
 		return PERCENT - hit;
 	}
 	
-	// Braden - Added the graphical effect for Fireball Scroll per requiremnt doc
+
 	public void graphicalEffect() {
 		System.out.println("A large ball of flame hurtles toward the\n"
 		        + "target.");
