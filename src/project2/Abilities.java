a05452ff96929439206dfe99c64361d963ea21c2
Nicholas Rodriguez <dub7@txstate.edu>
2023-10-27 09:55:09 -0500

    Added comments to abilities.java

--- src/project2/Abilities.java
+++ src/project2/Abilities.java
@@ -1,11 +1,13 @@
 package project2;
 
 public interface Abilities {
-    static final int PERCENT = 100;
+    static final int PERCENT = 100;     //Constant percent field set to 100.
+                                        //  Represents characters total hitPoints.
     
-    int calculateDamage(int hit);
+    int calculateDamage(int hit);       //Represents the percent of a 
+                                        //  character’s hitPoints to take.
     
-    void graphicalEffect();
-    
+    void graphicalEffect();             //Represents the visuals associated 
+                                        //  with an ability.
     
 }
