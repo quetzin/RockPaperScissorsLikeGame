6c4f3934e110f42a98daf278b2d08955cb9ec227
b.schlueter <schlueter.braden@gmail.com>
2023-10-27 15:48:32 -0500

    added javadoc

---
+++ src/project2/JavaDoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"p":"project2","l":"Abilities"},{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"project2","l":"BaseCharacter"},{"p":"project2","l":"FireballScroll"},{"p":"project2","l":"MagicCharacter"},{"p":"project2","l":"PlayerData"},{"p":"project2","l":"Project2"}];updateSearchResults();
