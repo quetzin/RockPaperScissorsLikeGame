8341c167fef3eb6a97ce3ac7e7800d4bb311c419
Nicholas Rodriguez <dub7@txstate.edu>
2023-10-27 15:27:24 -0500

    Added javadoc for MagicCharacter.java

--- src/project2/MagicCharacter.java
+++ src/project2/MagicCharacter.java
@@ -1,28 +1,37 @@
 /**
-Braden Schlueter    plr61
-Ibrahim Qafisheh    nmq6
-Nicholas Rodriguez  dub7
-Quetzin Pimentel    q_p12 
-*/
+ * @author Braden Schlueter (plr61)
+ * @author Ibrahim Qafisheh (nmq6)
+ * @author Nicholas Rodriguez (dub7)
+ * @author Quetzin Pimentel (q_p12)
+ */
 package project2;
 
 public class MagicCharacter extends BaseCharacter{
-	
-	private FireballScroll fireball;
-	
-	public FireballScroll getFireballScroll() {
-		return fireball;
-	}
-	
-	public MagicCharacter() {
-	    super(null, 70, "light");
-		fireball = new FireballScroll();
-	}
+    
+    private FireballScroll fireball;
+    
+    public FireballScroll getFireballScroll() {
+        return fireball;
+    }
+    
+    /**
+    * Construct a MagicCharacter object. Initializes the character with no
+    * name, 70 hit points, and a "light" alignment. It also creates a 
+    * FireballScroll for the character.
+    */
+    public MagicCharacter() {
+        super(null, 70, "light");
+        fireball = new FireballScroll();
+    }
 
-	
-    @Override
+    /**
+     * Get the class type of this character, which is "Magic."
+     *
+     * @return The class type of the character.
+     */
+     @Override
     public String getClassType() {
         return "Magic";
     }
-	
+    
 }
