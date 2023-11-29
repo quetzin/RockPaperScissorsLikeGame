a8beef264fd2d34585d6b669a1334fa33785b5eb
b.schlueter <schlueter.braden@gmail.com>
2023-11-28 21:00:00 -0600

    removed helper methods, winrate and name updates now

--- src/project3/GraphicalUserInterface.java
+++ src/project3/GraphicalUserInterface.java
@@ -19,77 +19,71 @@ import javax.swing.ListSelectionModel;
 
 
 
-public class GraphicalUserInterface extends JFrame implements ActionListener{
-    
-          
+public class GraphicalUserInterface extends JFrame implements ActionListener {
+
     private static final long serialVersionUID = 1L;
     JButton luteBtn, fireballBtn, shieldBtn;
-    JPanel lp,rp;
-    JList<String> playerList; //Should pass a String instead
+    JPanel lp, rp;
+    JList<String> playerList; // Should pass a String instead
     JLabel playerNameLabel, winRateLabel;
     private ArrayList<PlayerData> playerDataList;
-    
-    //----------------------------------------------------
-    public GraphicalUserInterface(ArrayList<PlayerData> playerDataList) {           //Constructor for GUI
+
+    // Constructor for the GUI
+    public GraphicalUserInterface(ArrayList<PlayerData> playerDataList) {
         this.playerDataList = playerDataList;
         JFrame frame = new JFrame("Fireball | Shield | Lute");
         JSplitPane sp = new JSplitPane();
         frame.add(sp);
         sp.setDividerLocation(300);
 
-        
-     // Create oDefaultListModel and populate it with PlayerData
+        // Creating a DefaultListModel and populating it with PlayerData
         DefaultListModel<String> listModel = new DefaultListModel<>();
         for (PlayerData playerData : playerDataList) {
             if (playerData.getIsActive()) {
                 listModel.addElement(playerData.getFirstName() + " " + playerData.getLastName());
             }
         }
-        
+
+        // Creating JList and setting selection mode
         playerList = new JList<>(listModel);
         playerList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-        
+
+        // Initializing JLabels for player information
         playerNameLabel = new JLabel("Player Name:");
         winRateLabel = new JLabel("Win Rate: ");
-        
 
-        
-       // Creates the playerList panel
+        // Creating the left panel with the player list and information labels
         lp = new JPanel(new BorderLayout());
-        // This adds the playerList to the top of the leftPane
         lp.add(new JScrollPane(playerList), BorderLayout.NORTH);
-        // labelsPanel is for Winrate and playerName
-        JPanel labelsPanel = new JPanel();
-        labelsPanel.setLayout(new BoxLayout(labelsPanel, BoxLayout.Y_AXIS));
-        
-        // adds both labels to the labelPanel
-        labelsPanel.add(playerNameLabel);
-        labelsPanel.add(winRateLabel);
-        
-        // This allows for both of the labels to be positioned vertically at the bottom
-        lp.add(labelsPanel, BorderLayout.SOUTH);
-        
-        // Add the left pane to the left side of the split
-        add(lp, BorderLayout.WEST);
-        
 
+        JPanel labelsPanel = new JPanel();
+        labelsPanel.setLayout(new BorderLayout());
+        labelsPanel.add(playerNameLabel, BorderLayout.NORTH);
+        labelsPanel.add(winRateLabel, BorderLayout.CENTER);
+
+        lp.add(labelsPanel, BorderLayout.SOUTH);
+        add(lp, BorderLayout.WEST);
+
+        // Handling player selection event
         playerList.addListSelectionListener(e -> {
             String selectedPlayer = playerList.getSelectedValue();
             if (selectedPlayer != null) {
                 PlayerData selected = getPlayerByName(playerDataList, selectedPlayer);
                 if (selected != null) {
-                playerNameLabel.setText(selected.getFirstName());
-                winRateLabel.setText("Win Rate: " + selected.getWinRate() + "%");
+                    playerNameLabel.setText("Player Name: " + selected.getFirstName());
+                    winRateLabel.setText("Win Rate: " + selected.getWinRate() + "%");
                 }
             }
         });
-       
+        
+        
 
-        // Add buttons 
+        
         luteBtn = new JButton("Lute"); //Text for button
         fireballBtn = new JButton("Fireball");
         shieldBtn = new JButton("Shield");
 //----------------------------------------------------
+        // Creating buttons for different actions
         luteBtn.setBounds(95, 20, 90, 40); // Set the bounds for the yellow button
         luteBtn.setBackground(Color.yellow); // Set the background color of the button to yellow
         luteBtn.addActionListener(this); // Add an action listener to the yellow button
@@ -98,18 +92,20 @@ public class GraphicalUserInterface extends JFrame implements ActionListener{
         fireballBtn.setBackground(Color.red);
         fireballBtn.addActionListener(this);
         
+        // Handling button click events
         shieldBtn.setBounds(95, 120, 90, 40);
         shieldBtn.setBackground(Color.blue);
         shieldBtn.addActionListener(this);
  //---------------------------------------------------   
         
+        // Creating the right panel and setting components
         rp = new JPanel(null);
         rp.add(luteBtn); //Puts button on right side
         rp.add(fireballBtn);
         rp.add(shieldBtn);
  //---------------------------------------------------    
         
-        
+        // Setting up the JFrame properties
         sp.setLeftComponent(lp);
         sp.setRightComponent(rp);
         frame.setSize(600, 400);
@@ -117,27 +113,34 @@ public class GraphicalUserInterface extends JFrame implements ActionListener{
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);     
     }
     
+    // ActionListener implementation for button clicks
     @Override
     public void actionPerformed(ActionEvent e) {
-        String selectedPlayer = playerList.getSelectedValue();
-        if (selectedPlayer != null) {
-            PlayerData selected = getPlayerByName(playerDataList, selectedPlayer);
-            if (selected != null) {
-                if (e.getSource() == fireballBtn) {
-                    // perform action for fireball btn using 'selected'
-                    System.out.println("Fireball button clicked for player: " + selected.getFirstName());
-                } else if (e.getSource() == shieldBtn) {
-                    // perform action for shieldBtn using 'selected'
-                    System.out.println("Shield button clicked for player: " + selected.getFirstName());
-                } else if (e.getSource() == luteBtn) {
-                    // Perform action for lute button using 'selected'
-                    System.out.println("Lute button clicked for player: " + selected.getFirstName());
-                }
+        if (playerList.getSelectedValue() != null) {
+            PlayerData selected = getPlayerByName(playerDataList, playerList.getSelectedValue());
+            if (e.getSource() == fireballBtn) {
+                // perform action for fireball btn
+                System.out.println("testing");
+            } else if (e.getSource() == shieldBtn) {
+                // perform action for shieldBtn
+            } else if (e.getSource() == luteBtn) {
+                // Perform action for lute button
             }
         }
     }
 
+    // Helper method to get player by name
+    private PlayerData getPlayerByName(ArrayList<PlayerData> playerDataList, String name) {
+        for (PlayerData player : playerDataList) {
+            if (player.getIsActive() && (player.getFirstName() + " " + player.getLastName()).equals(name)) {
+                return player;
+            }
+        }
+        return null;
+    }
+}
 
+    /*
     public ArrayList<PlayerData> getPlayerDataList() {
         return playerDataList;
     }
@@ -145,16 +148,4 @@ public class GraphicalUserInterface extends JFrame implements ActionListener{
     public void setPlayerDataList(ArrayList<PlayerData> playerDataList) {
         this.playerDataList = playerDataList;
     }
-    
-    private PlayerData getPlayerByName(ArrayList<PlayerData> playerDataList, String name) {
-        for (PlayerData player : playerDataList) {
-            if (player.getIsActive() && player.getFirstName().equals(name)) {
-                return player;
-            }
-            
-         }
-        return null;
-       }
-    
-
-}
+    */
